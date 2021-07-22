662bf054ee64a07bbd405954766fadcc41a1eee5
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-07-22 02:52:06 +0200

    Setting Up the Vidly Project.

---
+++ src/services/fakeMovieService.js
@@ -0,0 +1,142 @@
+// import Movies from '../components/movies';
+import genre from './fakeGenreService';
+
+export const getMovies = [
+  {
+    _id:"6035a4e105c319a9880as252b1n",
+    title:"Terminal",
+    genre:{     
+        _id:"6035a4e105319a9880a52b16",
+        name:"Action"
+        },
+    numberInstock:7,
+    dailyRentalRate:2.5,
+    published:"24 février 2021",
+    like:true
+  },
+  {
+    _id:"6035a4e105319a9c8w80as252b1n",
+    title:"The Avengers",
+    genre:{     
+     _id:"6035a4e105319a9880a52b16",
+     name:"Action"
+        },
+    numberInstock:3,
+    dailyRentalRate:2.5,
+    published:"24 février 2021",
+    like:true
+  },
+  {
+    _id:"6035a4e105319a98s80as252b1n",
+    title:"The sixth sense",
+    genre:{     
+     _id:"6035a4e105319a9d880a52d16",
+     name:"Cartoon"
+        },
+    numberInstock:6,
+    dailyRentalRate:2.5,
+    published:"24 février 2021",
+    like:true
+  },
+   
+  {
+    _id:"6035a4e105319a98s80was252b1n",
+    title:"Wedding crash",
+    genre:{     
+     _id:"6035a4e105319a98s80a5d2mi16",
+     name:"comedy"
+        },
+    numberInstock:2,
+    dailyRentalRate:2.5,
+    published:"24 février 2021",
+    like:true
+},
+{
+    _id:"6035a4e105319a98s80swas252b1n",
+   title:"Gone Girl",
+   genre:{     
+    _id:"6035a4e10fd5319a98s80sa5d2mi16",
+    name:"Thriller"
+       },
+   numberInstock:4,
+   dailyRentalRate:2.5,
+   published:"24 février 2021",
+   like:true
+},
+{
+    _id:"6035a4e105s319a98s80swas252b1n",
+   
+   title:"Gone Girl for me",
+   genre:{     
+    _id:"6035a4e10fd5319a98s80sa5d2mi16",
+    name:"Thriller"
+       },
+   numberInstock:9,
+   dailyRentalRate:2.5,
+   published:"24 février 2021",
+   like:true
+   },
+   {
+    _id:"6035a4e105319a9xs8s80swas252b1n",
+   
+   title:"Super hero",
+   genre:{     
+    _id:"6035a4e105319a9880a52b16",
+    name:"Action"  
+       },
+   numberInstock:2,
+   dailyRentalRate:2.5,
+   published:"24 février 2021",
+   like:true
+   },
+   {
+    _id:"6035wwa4e105319a9xs8s80swas252b1n",
+   
+   title:"Uncle dree",
+   genre:{     
+    _id:"6035a4e105319a98s80a5d2mi16",
+    name:"comedy"
+       },
+   numberInstock:6,
+   dailyRentalRate:2.5,
+   published:"24 février 2021",
+   like:true
+   },
+   {
+    _id:"60ss35wwa4e105319a9xs8s80swas252b1n",
+   
+   title:"America 2",
+   genre:{     
+    _id:"6035a4e10fd5319a98s80sa5d2mi16",
+    name:"Thriller"
+       },
+   numberInstock:7,
+   dailyRentalRate:2.5,
+   published:"24 février 2021",
+   like:true
+   },
+];
+
+
+// export function getMoviese() {
+//     return getMovies;
+// }
+export function getMoviese() {
+    return getMovies;
+}
+
+export function saveMovie(movie){
+    const newMovieInDB = getMovies.find(m=>m._id === movie._id) || {}
+    newMovieInDB.title = movie.title
+    newMovieInDB.numberInstock = movie.numberInstock
+    newMovieInDB.dailyRentalRate = movie.dailyRentalRate
+    newMovieInDB.genre = genre.find(g=>g._id === movie.genreId)
+    if(!newMovieInDB._id){
+        newMovieInDB._id = Date.now().toString()
+        getMovies.push(newMovieInDB)
+    }
+    return newMovieInDB
+   }
+export function getMovie(id) {
+    return getMovies.find((m) => m._id === id)
+}
